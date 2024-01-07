8c0b2f5de4a7450bb4191b6347e6593d30593071
MohammadReza HejaziRad <hejazi00831@gmail.com>
2024-01-07 21:16:21 +0330

    update docker app

--- src/modules/questions/dto/speaking-question-answer.dto.ts
+++ src/modules/questions/dto/speaking-question-answer.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class SpeakingQuestionAnswerDto {
   @ApiProperty({ required: false })
+  @IsOptional()
   @IsString()
   description?: string;
 }
